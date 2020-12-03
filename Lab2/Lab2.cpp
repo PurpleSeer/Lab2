dd73ccb5bd03ac32af31590ac44e15a66be6ed74
PurpleSeer <ilya.khristolyubov@yandex.ru>
2020-12-03 23:29:31 +0500

    1.2

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -38,26 +38,73 @@ void Point::reset() {
     y = 0;
 }
 
+class ColoredPoint : public Point{
+protected:
+    int color;
+public:
+    ColoredPoint() : Point() {
+        cout << "ColoredPoint\n";
+        color = 0;
+    }
+    ColoredPoint(int x, int y, int color) : Point(x,y) {
+        cout << "ColoredPoint(int x,int y)\n";
+        this->color = color;
+    }
+    ColoredPoint(const ColoredPoint& p) {
+        cout << "ColoredPoint(const Point &p)\n";
+        color = p.color;
+        x = p.x;
+        y = p.y;
+    }
+    ~ColoredPoint() {
+        cout << x << ", " << y <<", color="<<color<< " ";
+        cout << "~ColoredPoint()\n";
+    }
+    void change_color(int new_color) {
+        color = new_color;
+    }
+};
+
 int main()
 {
     {
-    Point p;
-    Point p2(10, 20);
-    Point p3(p2);
-    }   
+        Point p;
+        Point p2(10, 20);
+        Point p3(p2);
+    }
+
+    _getch();
+    cout << "\n\n";
     
     Point* p4 = new Point;
     Point* p5 = new Point(10, 20);
     Point* p6 = new Point(*p5);
-
+    p4->reset();
+    p4->move(10, 10);
     delete p4;
     delete p5;
     delete p6;
-    
-    Point* p7 = new Point(1, 2);
-    p7->reset();
-    p7->move(10, 10);
-    delete p7;
+
+    _getch();
+    cout << "\n\n";
+
+    {
+        ColoredPoint cp;
+        ColoredPoint cp2(10, 20, 32);
+        ColoredPoint cp3(cp2);
+    }
+
+    _getch();
+    cout << "\n\n";
+
+    Point* cp4 = new ColoredPoint();
+    delete cp4;
+    ColoredPoint* cp5 = new ColoredPoint;
+    ColoredPoint* cp6 = new ColoredPoint(10, 20, 32);
+    ColoredPoint* cp7 = new ColoredPoint(*cp6);
+    delete cp5;
+    delete cp6;
+    delete cp7;
 
     _getch();
     return 0;
